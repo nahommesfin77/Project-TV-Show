cef0ca05469178e78ef01b01f86c2ae251765df9
Nahom Mesfin <mnahom545@gmail.com>
2025-08-18 11:40:21 +0100

    add episode title and image

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ function makePageForEpisodes(episodeList) {
     // Create container div for each episode
     const episodeDiv = document.createElement("div");
     episodeDiv.className = "episode-card";
- }
+ 
+        // Create episode code S02E07 style
+    const seasonCode = String(episode.season).padStart(2, "0");
+    const numberCode = String(episode.number).padStart(2, "0");
+    const episodeCode = `S${seasonCode}E${numberCode}`;
+ 
+    // Episode title
+    const title = document.createElement("h2");
+    title.textContent = `${episode.name} (${episodeCode})`;
+
+    // Episode image
+    const img = document.createElement("img");
+    img.src = episode.image.medium;
+    img.alt = `${episode.name} image`;})
